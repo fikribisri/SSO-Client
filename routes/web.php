263b8e4bb484a2eeccc70ee336cfff8aabe7413b
Fikri Bisri <fikri@ndt.co.id>
2022-05-31 13:18:31 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\SSO\SSOController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
